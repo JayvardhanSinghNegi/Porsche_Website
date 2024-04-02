c0bdbf5c5b9f66775333ca2312124b18833ce651
JayvardhanSinghNegi <143247533+JayvardhanSinghNegi@users.noreply.github.com>
2024-04-02 17:40:23 +0530

    Add files via upload

---
+++ car_web/script.js
@@ -0,0 +1,15 @@
+function openCarPage(url) {
+    window.open(url, '_blank');
+}
+
+function openMerchandisePage() {
+    window.open('https://shop.porsche.com/us/en-US', '_blank');
+}
+
+function openModelsPage() {
+    window.open('https://www.porsche.com/middle-east/_india_/modelstart/all/', '_blank');
+}
+
+function openloginpage() {
+    window.open('https://www.porsche.com/middle-east/_india_/modelstart/all/', '_blank');
+}
